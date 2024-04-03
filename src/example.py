1bc390d509b45952ecb975a604dad3f2c3ac7e88
ansh-srivastava <ansh.srivastava0987@gmail.com>
2024-04-03 22:18:27 +0530

    comminting

--- src/example.py
+++ src/example.py
@@ -7,9 +7,9 @@ print(my_list) # output = [1, 2, 3, 4, 5]
 # how to reverse the list 
 
 emp_list = []
-# length = len(my_list)
-# for i in range(length,0,-1):
-#     emp_list.append(i)
-# print(emp_list)
-emp_list = my_list[::-1]
+length = len(my_list)
+for i in range(length,0,-1):
+    emp_list.append(i)
+print(emp_list)
+
 print(emp_list)
